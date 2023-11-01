aed5b0cf9bc7047d317762829d547f4c16b8c4e7
James Dempsey <jamesdempsey07@gmail.com>
2023-11-01 21:03:01 +0000

    add new tickets

--- booking_success/script.js
+++ booking_success/script.js
@@ -4,26 +4,58 @@ const searchParams = new URLSearchParams(url.search);
 
 var total = 0.00;
 
-if (searchParams.size === 0) {
-    document.location = "../buy/"
-}
+change = false;
 
-if (searchParams.get("normal-tickets")) {
+if (searchParams.size > 0) {
+if (searchParams.get("normal-tickets") && searchParams.get("normal-tickets") > 0) {
     
     ticketsEle = document.getElementById("tickets");
-    total = 69.99 * searchParams.get("normal-tickets")
+    total = 50 * searchParams.get("normal-tickets")
 
     newEle = document.createElement("li")
-    newPrice = Math.ceil((Number(69.99*searchParams.get("normal-tickets")))*100)/100
-    if ((newPrice*100)%10 == 0) {
-        newPrice = String(newPrice) + "0"
-    }
+    newPrice = Math.ceil((Number(50*searchParams.get("normal-tickets")))*100)/100
+
     
     
     newEle.innerText = `${searchParams.get("normal-tickets")}x Normal Ticket (£${newPrice})`
     ticketsEle.appendChild(newEle)
+    change = true
 }
 
+if (searchParams.get("paintballing-tickets") && searchParams.get("paintballing-tickets") > 0) {
+    
+    ticketsEle = document.getElementById("tickets");
+    total += 12 * searchParams.get("paintballing-tickets")
+
+    newEle = document.createElement("li")
+    newPrice = Math.ceil((Number(12*searchParams.get("paintballing-tickets")))*100)/100
+
+    
+    
+    newEle.innerText = `${searchParams.get("paintballing-tickets")}x Paintballing Ticket (£${newPrice})`
+    ticketsEle.appendChild(newEle)
+    change = true
+}
+
+if (searchParams.get("zorbing-tickets") && searchParams.get("zorbing-tickets") > 0) {
+    
+    ticketsEle = document.getElementById("tickets");
+    total += 10 * searchParams.get("zorbing-tickets")
+
+    newEle = document.createElement("li")
+    newPrice = Math.ceil((Number(10*searchParams.get("zorbing-tickets")))*100)/100
+
+    
+    
+    newEle.innerText = `${searchParams.get("zorbing-tickets")}x Zorbing Ticket (£${newPrice})`
+    ticketsEle.appendChild(newEle)
+    change = true
+}
+}
+
+if (!change) {
+    document.location = "../buy/"
+}
 
 // update total
 document.getElementById("total").innerText = "Total: £"+total;
